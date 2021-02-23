de71d1f036597ed338e4c04cc3f9f67a31778fae
BuildTools <unconfigured@null.spigotmc.org>
2021-02-23 20:03:33 +0100

    simplified process stdin/ out passing code

--- tests/servercommunication.py
+++ tests/servercommunication.py
@@ -3,25 +3,27 @@ import threading
 import sys
 
 
-class Communicator(threading.Thread):
-    def __init__(self, process):
-        super().__init__()
-        self.process: subprocess.Popen = process
+def print_stdout(proc):
+    while proc.poll() is None:
+        for stdline in iter(proc.stdout.readline, ""):
+            sys.stdout.write(stdline.decode())
 
-    def run(self) -> None:
-        while self.process.poll() is None:
-            for stdline in iter(self.process.stdout.readline, ""):
-                sys.stdout.write(stdline.decode())
+
+def redirect_input(proc):
+    while proc.poll() is None:
+        s = input("> ")
+        proc.stdin.write(s.encode("ascii") + b'\n')
+        proc.stdin.flush()
 
 
 proc = subprocess.Popen("java -Xmx2G -jar server.jar --nogui", stdin=subprocess.PIPE, stdout=subprocess.PIPE, cwd="./run")
 
-communicator = Communicator(proc)
-communicator.start()
+output_redirector = threading.Thread(target=print_stdout, args=[proc])
+input_redirector = threading.Thread(target=redirect_input, args=[proc], daemon=True)
+output_redirector.start()
+input_redirector.start()
 
 while proc.poll() is None:
-    s = input("> ")
-    proc.stdin.write(s.encode("ascii") + b'\n')
-    proc.stdin.flush()
+    pass
 
 print("ended")
