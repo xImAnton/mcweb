04a7aaf0f016606d64ff03c0b0f351d22dad107f
BuildTools <unconfigured@null.spigotmc.org>
2021-02-26 09:51:40 +0100

    added docstrings

--- mcweb/io/database.py
+++ mcweb/io/database.py
@@ -3,6 +3,9 @@ from ..config import Config
 
 
 class DatabaseConnector(Database):
+    """
+    class for making database calls
+    """
     def __init__(self, db_path):
         super().__init__(f"sqlite:///{db_path}")
         self.db_path = db_path
@@ -17,6 +20,13 @@ class DatabaseConnector(Database):
                 await self.execute(f.read())
 
     async def update(self, table, condition, values):
+        """
+        shorthand method for sql UPDATE statement
+        :param table: the table the data is in
+        :param condition: the condition to detect the records to be changed
+        :param values: dict of field names/ values to update
+        :return:
+        """
         kvalues = []
         for k, v in values.items():
             kvalues.append(" = ".join((k, str(v))))
--- mcweb/io/wsmanager.py
+++ mcweb/io/wsmanager.py
@@ -3,16 +3,32 @@ import asyncio
 
 
 class WebsocketConnectionManager:
+    """
+    class for managing all websocket connections to a MinecraftServer
+    """
     def __init__(self):
         self.connections = []
 
     async def connected(self, ws):
+        """
+        registers a websocket to the manager
+        :param ws: the websocket to register
+        """
         self.connections.append(ws)
 
     async def disconnected(self, ws):
+        """
+        unregisters a websocket connection to the server
+        :param ws: the websocket to unregister
+        """
         self.connections.remove(ws)
 
     async def broadcast(self, msg, except_=None):
+        """
+        broadcasts a message to all connections
+        :param msg: the msg to broadcast
+        :param except_: a websocket the packet shouldn't be sent to
+        """
         for ws in self.connections:
             if ws == except_:
                 continue
@@ -22,4 +38,7 @@ class WebsocketConnectionManager:
                 await self.disconnected(ws)
 
     def broadcast_sync(self, msg, except_=None):
+        """
+        sync call for coroutine broadcast
+        """
         asyncio.run(self.broadcast(msg, except_))
