a146853230f882345af366e5ef46c335ded88845
Anton Vogelsang <anton.vogelsang@outlook.de>
2021-03-30 13:00:35 +0200

    fixed database uri encoding issue that led to authentication error

--- mcweb/io/mongo.py
+++ mcweb/io/mongo.py
@@ -1,12 +1,13 @@
 import motor.motor_asyncio
 from mcweb.io.config import Config
+import urllib.parse
 
 
 class MongoClient(motor.motor_asyncio.AsyncIOMotorClient):
     def __init__(self, mc, loop):
-        username = Config.get_docker_secret("mongo_user") or Config.MONGO['username']
-        password = Config.get_docker_secret("mongo_password") or Config.MONGO['password']
-        uri = f"mongodb://{username}:{password}@{Config.MONGO['host']}:{Config.MONGO['port']}/admin"
+        username = urllib.parse.quote_plus(Config.get_docker_secret("mongo_user") or Config.MONGO['username'])
+        password = urllib.parse.quote_plus(Config.get_docker_secret("mongo_password") or Config.MONGO['password'])
+        uri = f"mongodb://{username}:{password}@{Config.MONGO['host']}:{Config.MONGO['port']}/mcweb?authSource=admin"
         super(MongoClient, self).__init__(uri, io_loop=loop)
         self.mc = mc
         self.db = self[Config.MONGO["database"]]
