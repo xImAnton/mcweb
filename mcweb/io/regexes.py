6a3bd7b9a99116fd20e365010eab40d92f97df98
xImAnton <anton.vogelsang@outlook.de>
2021-04-11 12:58:26 +0200

    removed length restrictions for server names

--- mcweb/io/regexes.py
+++ mcweb/io/regexes.py
@@ -3,6 +3,6 @@ import re
 
 class Regexes:
     DONE = re.compile(r'^(\[[0-9]+:[0-9]+:[0-9]+ .*\]: Timings Reset)|(\[[0-9]*:[0-9]*:[0-9]*\] \[Server thread\/INFO\]: Time elapsed: [0-9]* ms)|(\[[0-9]*:[0-9]*:[0-9]*\] \[.*\]: Done \([0-9]*[\.,][0-9]*s\)! For help, type "help"( or "\?")?)$')
-    SERVER_DISPLAY_NAME = re.compile(r"^[a-zA-Z0-9_\-\. ]{3,48}$")
+    SERVER_DISPLAY_NAME = re.compile(r"^[a-zA-Z0-9_\-\. ]*$")
     USER_NAME = re.compile(r"^[a-z0-9A-Z_-]{6,15}$")
     USER_MAIL = re.compile(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$")
